68316b5a631e3daa0e96039bfa08e65bac674f56
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-05-04 21:11:57 +0200

    Additional logging

--- RenderDistanceExtender/dllmain.cpp
+++ RenderDistanceExtender/dllmain.cpp
@@ -24,12 +24,16 @@ DWORD WINAPI PatchThread(LPVOID) {
 
                 api->SetAddress(addr_fog_distance, 0x7FFFFFFF);
                 api->SetAddress(addr_render_distance, 0x7FFFFFFF);
+
+                api->Log("Render distance extended!");
             }
 
             // Deactivate
             else {
                 api->SetAddress(addr_fog_distance, original_fog_distance);
                 api->SetAddress(addr_render_distance, original_render_distance);
+
+                api->Log("Render distance reverted!");
             }
 
             is_active = !is_active;
