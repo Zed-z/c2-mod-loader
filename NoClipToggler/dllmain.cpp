00bac9604399c7a0ad965ac9737a2fc02e88426b
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-05-11 02:20:48 +0200

    Noclip toggler tweaks

--- NoClipToggler/dllmain.cpp
+++ NoClipToggler/dllmain.cpp
@@ -20,6 +20,11 @@ const uintptr_t jumpFalloffCode = 0x0047FDCF; // add ecx, -14
 const BYTE jumpFalloffCodeOriginal[] = { 0x83, 0xC1, 0xEC };
 const BYTE jumpFalloffCodePatch[] = { 0x90, 0x90, 0x90 };
 
+// Disable fall timer (to prevent death)
+const uintptr_t fallTimerCode = 0x00488162; // add [eax], 00001000
+const BYTE fallTimerCodeOriginal[] = { 0x81, 0x00, 0x00, 0x10, 0x00, 0x00 };
+const BYTE fallTimerCodePatch[] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+
 bool noclip_enabled = false;
 int noclip_y = 0;
 
@@ -39,13 +44,15 @@ DWORD WINAPI PatchThread(LPVOID) {
             if (noclip_enabled) {
                 api->PatchBytes(patchAddress, patchBytes, sizeof(patchBytes));
                 api->PatchBytes(fallingCode, fallingCodePatch, sizeof(fallingCodePatch));
-                //api->PatchBytes(jumpFalloffCode, jumpFalloffCodePatch, sizeof(jumpFalloffCodePatch));
+                api->PatchBytes(jumpFalloffCode, jumpFalloffCodePatch, sizeof(jumpFalloffCodePatch));
+                api->PatchBytes(fallTimerCode, fallTimerCodePatch, sizeof(fallTimerCodePatch));
                 api->Log("Noclip enabled!");
             }
             else {
                 api->PatchBytes(patchAddress, originalBytes, sizeof(originalBytes));
                 api->PatchBytes(fallingCode, fallingCodeOriginal, sizeof(fallingCodeOriginal));
-                //api->PatchBytes(jumpFalloffCode, jumpFalloffCodeOriginal, sizeof(jumpFalloffCodeOriginal));
+                api->PatchBytes(jumpFalloffCode, jumpFalloffCodeOriginal, sizeof(jumpFalloffCodeOriginal));
+                api->PatchBytes(fallTimerCode, fallTimerCodeOriginal, sizeof(fallTimerCodeOriginal));
                 api->Log("Noclip disabled!");
             }
         }
@@ -53,11 +60,11 @@ DWORD WINAPI PatchThread(LPVOID) {
         // Go up and down
         if (noclip_enabled) {
             if (GetAsyncKeyState(VK_PRIOR)) {// Page up
-                noclip_y += 10;
+                noclip_y += 50;
             }
 
             if (GetAsyncKeyState(VK_NEXT) & 1) {// Page down
-                noclip_y -= 10;
+                noclip_y -= 100;
             }
 
             auto addr = api->ResolveAddress(ADDR_CROC_POS_Y);
