9e80e28fa467efea2632060cbe2d983fef24a6b8
Zed-z <71879051+Zed-z@users.noreply.github.com>
2025-08-15 21:50:17 +0200

    Speedrun utils basics

---
+++ SpeedrunUtils/dllmain.cpp
@@ -0,0 +1,27 @@
+#include "ModApi.h"
+#include <Windows.h>
+#include <iostream>
+
+ModApi* api = nullptr;
+
+void __stdcall resetBossFlags() {
+    api->AddressSetInt(0x622D50, 0);
+    api->ShowToast("Reset boss flags successfully.");
+}
+
+MenuActionRegistration __stdcall resetBossFlagsRegistration() {
+    return { "Reset boss flags", "Reset boss warp flags.", resetBossFlags, true};
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        api = LoadModApi();
+        if (!api) return FALSE;
+
+		// Register menu action
+        api->RegisterMenuAction(hModule, resetBossFlagsRegistration);
+
+        DisableThreadLibraryCalls(hModule);
+    }
+    return TRUE;
+}
